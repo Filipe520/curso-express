e569570b535b25eb954d9be9d74f6e850fed335c
Filipe520 <filipealves520@gmail.com>
2025-06-15 16:16:28 -0300

    trabalhando com rotas é instalando o Nodemon

--- app.js
+++ app.js
@@ -3,7 +3,21 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Olá, Mundo!");
+  res.send("Seja Bem-Vindo ao nosso site! TESTANDO O Nodemon");
+});
+
+app.get("/artigos/:id/:data", (req, res) => {
+  if (req.params.id == "1" && req.params.data == "18-04-2025") {
+    res.send("1 - Como Criar aplicativo Android");
+  } else if (req.params.id == "2") {
+    res.send("2 - Como usar o Node.js \n 3 - Como usar o Express");
+  } else {
+    res.send("Nenhum artigo foi encontrado!");
+  }
+});
+
+app.get("/contato", (req, res) => {
+  res.send("Deixe a sua mensagem");
 });
 
 app.listen(3000, console.log("Servidor está rodando 🚀"));
