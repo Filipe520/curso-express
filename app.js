4f617c3615255ee3204cc67aaea2a3cf885915ce
Filipe520 <filipealves520@gmail.com>
2025-06-15 15:52:29 -0300

    curso de express

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Olá, Mundo!");
+});
+
+app.listen(3000, console.log("Servidor está rodando 🚀"));
