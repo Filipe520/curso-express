2bfa33a5f7b550bdd8fc85e6e7c82760f28dc94d
Filipe520 <filipealves520@gmail.com>
2025-06-16 16:45:41 -0300

    configurando o Sequelize no arquivo db.js

---
+++ models/db.js
@@ -0,0 +1,19 @@
+// Importando a Lib
+import { Sequelize } from "sequelize";
+
+// Conectando ao Banco de dados
+const sequelize = new Sequelize("cadastro", "root", "123456", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+// Fazendo verificação do sequelize
+sequelize
+  .authenticate()
+  .then((res) => console.log("Banco de dados conectado com sucesso 🚀 ", res))
+  .catch((err) =>
+    console.log("Erro ao se conectar com o Banco de Dados, ", err)
+  );
+
+// Exportadando esse documento para acessar em outras áreas do projeto
+export { Sequelize, sequelize };
